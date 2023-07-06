e8c412e5c7118f131406106f4836366e747a71a9
Trey Foster <treybfoster@gmail.com>
2023-07-06 16:16:13 -0500

    add formatters

--- README.md
+++ README.md
@@ -6,5 +6,12 @@ Simply clone this repo and run the following:
 
 ```bash
 yarn || npm i
+composer install
 yarn dev && php artisan serve
+```
+
+This package also includes opnionated formatters like pint and rome if you are into that! To run these formatters simply run the following command.
+
+```bash
+yarn format  || npm run format
 ```
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
-        return pages[`./Pages/${name}.tsx`]
-    },
-    setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
-    },
-})
+	resolve: (name) => {
+		const pages = import.meta.glob("./Pages/**/*.tsx", { eager: true });
+		return pages[`./Pages/${name}.tsx`];
+	},
+	setup({ el, App, props }) {
+		createRoot(el).render(<App {...props} />);
+	},
+});
